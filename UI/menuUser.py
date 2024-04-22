76f575a0e91ce37f36a1d896b0278b5b20dba84e
Dmitry Churilov <churilov.dmitrycd@mail.ru>
2024-04-22 10:44:13 +0300

    MainMenu

--- UI/menuUser.py
+++ UI/menuUser.py
@@ -2,7 +2,7 @@ import UI.menuHeader as m
 
 
 def menu_console():
-        m.printNenuTitle("Заметки\n           главное меню")
+        m.printMenuTitle("Заметки\n           главное меню")
         print("1 - вывод журнала \n2 - вывод заметки по id \n3 - выбор заметки по дате \n4 - редактирование заметки"
               " \n5 - добавление заметки \n6 - удаление заметки \n7 - выход из заметок")
         m.printMenuLine()
